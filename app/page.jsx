82a265f78bd1b518a9fc21cee8074e0d3aa3e8aa
michaelperry101 <michael.perry94@outlook.com>
2025-08-27 23:13:08 +0100

    Add files via upload

---
+++ app/page.jsx
@@ -0,0 +1,2 @@
+export const dynamic = 'force-static';
+export default function Success(){return <main className="doc"><h1>Success</h1><p>Thanks for subscribing! Your account will update shortly.</p></main>}
