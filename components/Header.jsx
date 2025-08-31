97afc23670552f73944f639d617d3dc5e1443f1a
michaelperry101 <michael.perry94@outlook.com>
2025-08-31 15:49:06 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,25 +1,39 @@
+// components/Header.jsx
 "use client";
-import { useSidebar } from "./SidebarContext";
+import Image from "next/image";
+import Link from "next/link";
+import { useSidebar } from "@/components/SidebarContext";
 
 export default function Header() {
   const { toggle } = useSidebar();
+
   return (
-    <header className="app-header">
-      <button
-        className="hamburger"
-        aria-label="Open menu"
-        onClick={toggle}
-        type="button"
-      >
-        ☰
-      </button>
+    <header className="header">
+      <div className="header-inner">
+        {/* hamburger (left) */}
+        <button
+          aria-label="Open menu"
+          className="hamburger"
+          onClick={toggle}
+          type="button"
+        >
+          <span />
+          <span />
+          <span />
+        </button>
 
-      <a href="/" className="logo-link" aria-label="HelpHub247 Home">
-        <img src="/logo.png" alt="HelpHub247" />
-      </a>
-
-      {/* right side spacer */}
-      <div style={{ width: 40 }} />
+        {/* centered logo (go home) */}
+        <Link href="/" className="logo-link" aria-label="Go to Home">
+          {/* expects /public/logo.png */}
+          <Image
+            src="/logo.png"
+            alt="HelpHub 24/7"
+            width={150}
+            height={40}
+            priority
+          />
+        </Link>
+      </div>
     </header>
   );
 }
